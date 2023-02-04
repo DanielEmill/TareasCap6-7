dfac278060b377340891a635c7f24b42663babb5
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-02-04 00:36:43 -0400

    Upload Task

---
+++ Cap6/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Agregar el cálculo del promedio para el programa con arreglo jagged.*/
+promedioEdad();
+
+const int CANTIDAD = 5;
+
+static void promedioEdad()
+	{
+	int[][] jagged = new int[1][];
+    jagged[0] = new int[CANTIDAD]{
+        10, 43, 62, 64, 57
+    };
+    int menor = 0, mayor = 0;
+	int promedio = 0, sumatoria = 0;
+    menor = jagged[0][0];
+    mayor = jagged[0][0];
+    calculos(mayor, menor, jagged, sumatoria, promedio);
+	}
+
+static void calculos(int mayor, int menor, int[][] jagged, int sumatoria, int promedio)
+{
+    for (int i = 0; i < jagged.Length; i++)
+    {
+        for (int j = 0; j < jagged[i].Length; j++)
+        {
+            sumatoria += jagged[i][j]; 
+            if(jagged[i][j] > mayor){
+                mayor = jagged[i][j];
+            }
+            if (jagged[i][j] < menor){
+                menor = jagged[i][j];
+            }
+        }
+    }
+    promedio = sumatoria / CANTIDAD;
+
+    System.Console.WriteLine("Promedio :" + promedio);
+    System.Console.WriteLine("Mayor :" + mayor);
+    System.Console.WriteLine("Menor :" + menor);
+}
---
+++ Cap7/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+
+
+Ejercicio1();
+Ejercicio2();
+Ejercicio3();
+/*Hacer el programa que calcula el
+promedio, calificación máxima y mínima
+de un salón de clases usando el ArrayList.
+*/
+static void Ejercicio1()
+	{
+        List <int> num = new List <int>();
+		int promedio = 0, sumatoria = 0, maxima = 0, minima = 0;
+
+
+        num.Add(5);
+        num.Add(56);
+        num.Add(57);
+        num.Add(55);
+        num.Add(85);
+
+        maxima = num[0];
+        minima = num[0];
+        for(int i = 0; i <num.Count; i++){
+            sumatoria += num[i];
+            if(num[i] > maxima){
+                maxima = num[i];
+            }
+            if(num[i]<maxima){
+                minima = num[i];
+            }
+        }
+        promedio = sumatoria / num.Count;
+        System.Console.WriteLine($"Promedio: " + promedio);
+        System.Console.WriteLine($"Maximo: " + maxima);
+        System.Console.WriteLine($"Minimo: " + minima);
+	}
+/*2 Hacer un programa que funcione como un
+diccionario, con palabra y definición,
+usando el Hashtable.*/
+static void Ejercicio2(){
+    Hashtable diccionario = new Hashtable();
+    diccionario.Add("Vida", "La vida ​ es un sistema químico que utiliza energía para evitar alcanzar el equilibrio químico.");
+    diccionario.Add("Big bang", "En cosmología, se entiende por Big Bang​​ o Gran Explosión​ en español​ al principio del universo, es decir, el punto inicial en el que se formó la materia, el espacio y el tiempo.​ De acuerdo con el modelo cosmológico estándar, el Big Bang tuvo lugar hace unos 13 800 millones de años.​");
+
+    System.Console.WriteLine($"El significado de Big bang: {diccionario["Big bang"]}");
+}
+
+/*5 Hacer un programa que funcione como
+una agenda telefónica y que guarde el
+nombre de la persona y su número
+telefónico.*/
+static void Ejercicio3(){
+    Hashtable agenda = new Hashtable();
+    agenda.Add("levi el muerto", 8498556235);
+    agenda.Add("delgado messi", 84424235);
+    agenda.Add("edgar el dao", 846542565);
+    agenda.Add("stive el que pega", 5136595642);
+    System.Console.WriteLine($"El numero de Marco {agenda["levi el muerto"]}");
+}
